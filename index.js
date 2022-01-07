41d3443d0edd0ee0f0892d5d962929db5fdac476
rocio ailen <ibrocio16@gmail.com>
2022-01-07 19:50:57 -0300

    mostrar detalles en tarjeta principal

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const personajesDisney = () => {
         .then((data) => {
             console.log(data)
             mostrarTarjetas(data)
-
+            clickPorTarjeta()
         })
 }
 personajesDisney()
@@ -27,4 +27,57 @@ const mostrarTarjetas = data => {
 `
     }, "")
     seccionTarjetas.innerHTML = html
+}
+
+const buscarPersonaje = (id) => {
+    fetch(`https://api.disneyapi.dev/characters/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            tarjetaDetalle(data)
+        })
+}
+
+
+const clickPorTarjeta = () => {
+    const tarjetas = document.querySelectorAll(".tarjetas-datos")
+
+    for (let i = 0; i < tarjetas.length; i++) {
+        tarjetas[i].onclick = () => {
+            const idPersonaje = tarjetas[i].dataset.id
+            buscarPersonaje(idPersonaje)
+
+            seccionTarjetas.style.display = "none"
+            conteinerBotones.style.display = "none"
+        }
+
+    }
+
+}
+
+const tarjetaDetalle = data => {
+
+    seccionDetalles.style.display = "flex"
+
+    seccionDetalles.innerHTML = `
+    <article>
+    <div id="menu-times">
+    <i class="fas fa-times"></i>
+</div>
+    <img src="${data.imageUrl}">
+    <h2>${data.name}</h2>
+    <p>Films: ${data.films.length === 0 ? "No results":data.films.join(', ')}</p>
+    <p>Short Films: ${data.shortFilms.length === 0 ? "No results":data.shortFilms.join(', ')}</p> 
+    <p>Tv Shows: ${data.tvShows.length === 0 ? "No results":data.tvShows.join(', ')}</p>
+    <p>Videogames: ${data.videoGames.length === 0 ? "No results":data.videoGames.join(', ')}</p> 
+    <p>Park Attraction: ${data.parkAttractions.length === 0 ? "No results":data.parkAttractions.join(', ')}</p> 
+    </article> `
+
+    const menuTimes = document.getElementById("menu-times")
+    menuTimes.onclick = () => {
+        seccionDetalles.style.display = "none"
+        menuTimes.style.display = "none"
+        seccionTarjetas.style.display = "flex"
+        conteinerBotones.style.display = "block"
+    }
 }
